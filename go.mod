120a7ad0a94188f50f401b8f3d04b22bef8b3a6a
NjiruClinton <njiruclinton56@gmail.com>
2023-10-24 00:00:45 +0300

    commands and documentation

--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module gfg
 go 1.19
 
 require (
-	github.com/BurntSushi/toml v1.3.2 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli v1.22.14 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/peterhellberg/tinypng v0.0.0-20190911095043-2ad76796e9b6 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,34 +1,138 @@
-// main.go
-
 package main
 
 import (
-	"flag"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
-	"tectonic_assets/commands"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	browseCmd := flag.NewFlagSet("browse", flag.ExitOnError)
-	optimizeCmd := flag.NewFlagSet("optimize", flag.ExitOnError)
+// Asset configuration
+type Assets struct {
+	Path string
+}
 
-	// Parse flags
-	switch os.Args[1] {
-	case "browse":
-		err := browseCmd.Parse(os.Args[2:])
-		if err != nil {
-			return
-		}
-		commands.BrowseAssets()
-	case "optimize":
-		err := optimizeCmd.Parse(os.Args[2:])
-		if err != nil {
-			return
-		}
-		commands.OptimizeAssets()
-	default:
-		log.Fatal("Unknown command")
+var assets Assets
+
+var rootCmd = &cobra.Command{
+	Use:   "flutter-asset",
+	Short: "Manage Flutter assets",
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&assets.Path, "path", "p", "assets", "Asset folder path")
+}
+
+
+var browseCmd = &cobra.Command{
+	Use:   "browse",
+	Short: "Browse assets folder",
+	Run: func(cmd *cobra.Command, args []string) {
+		assets.Browse()
+	},
+}
+
+var optimizeCmd = &cobra.Command{
+	Use:   "optimize",
+	Short: "Optimize image assets",
+	Run: func(cmd *cobra.Command, args []string) {
+		assets.OptimizeImages()
+	},
+}
+
+var manifestCmd = &cobra.Command{
+	Use:   "manifest",
+	Short: "Generate asset manifest",
+	Run: func(cmd *cobra.Command, args []string) {
+		assets.GenerateManifest()
+	},
+}
+
+
+func (a *Assets) Browse() {
+	files, err := ioutil.ReadDir(a.Path)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
+
+func (a *Assets) OptimizeImages() {
+
+	files, _ := ioutil.ReadDir(a.Path)
+
+	for _, file := range files {
+
+		if strings.HasSuffix(file.Name(), ".png") {
+
+			input := filepath.Join(a.Path, file.Name())
+			output := filepath.Join(a.Path, "optimized", file.Name())
+
+			err := exec.Command("tinypng", input, output).Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+
+	}
+
+}
+
+func GetAssetHash(name string) string {
+	// hash asset contents soon...
+	return "hash"
+}
+
+func (a *Assets) GenerateManifest() {
+
+	manifest := map[string]string{}
+
+	var manifestFile *os.File
+	manifestFile, err := os.Create("manifest.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, _ := ioutil.ReadDir(a.Path)
+
+	for _, file := range files {
+		name := file.Name()
+		hash := GetAssetHash(name)
+		manifest[name] = hash
+	}
+
+	json.NewEncoder(manifestFile).Encode(manifest)
+
+	manifestFile.Close()
+
+}
+
+// Hooks
+//TODO: dont forget about hooks
+
+func PostBuildHook() {
+	assets := Assets{Path: "build/assets"}
+	assets.GenerateManifest()
+}
+
+func main() {
+
+	rootCmd.AddCommand(browseCmd)
+	rootCmd.AddCommand(optimizeCmd)
+	rootCmd.AddCommand(manifestCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+
 }

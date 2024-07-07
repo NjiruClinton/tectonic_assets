b70882410493ae041253f197c8305cc81a271d55
Clinton Njiru <njiruclinton56@gmail.com>
2024-07-07 12:23:46 +0300

    access .env during builds

--- db/db.go
+++ db/db.go
@@ -25,7 +25,8 @@ func Pgdb() {
 		log.Fatalf("Error loading .env file")
 	}
 
-	connectionStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", goDotEnvVariable("USER"), goDotEnvVariable("PASSWORD"), goDotEnvVariable("DBNAME"), goDotEnvVariable("SSL_MODE"))
+	connectionStr := goDotEnvVariable("DB_CONNECTION_STRING")
+	//fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", goDotEnvVariable("USER"), goDotEnvVariable("PASSWORD"), goDotEnvVariable("DBNAME"), goDotEnvVariable("SSL_MODE"))
 	fmt.Println(connectionStr)
 
 	conn, err := sql.Open("postgres", connectionStr)
